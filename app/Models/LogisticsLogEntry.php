76fcd879eb52ba560de671488598a0f02d1bdaba
Cosmas <cosmasasango12@gmail.com>
2025-12-15 13:09:12 +0300

    image display

--- app/Models/LogisticsLogEntry.php
+++ app/Models/LogisticsLogEntry.php
@@ -14,6 +14,7 @@ class LogisticsLogEntry extends Model
         'departure',
         'setdown_time',
         'vehicle_allocated',
+        'driver',
         'project_officer_incharge',
         'remarks',
         'status',
