f4ae1662bdc80990c0066b8aa20f7e3254c6b4dc
ubuntucosmas <cosmasasango12@gmail.com>
2026-08-06 17:50:06 +0300

    fix(materials): map UOM model to registry table

--- app/Modules/MaterialsLibrary/Models/UnitOfMeasure.php
+++ app/Modules/MaterialsLibrary/Models/UnitOfMeasure.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class UnitOfMeasure extends Model
 {
+    protected $table = 'units_of_measure';
+
     protected $fillable = ['code', 'name', 'dimension', 'decimal_places', 'allows_fraction', 'is_active'];
     protected $casts = ['decimal_places' => 'integer', 'allows_fraction' => 'boolean', 'is_active' => 'boolean'];
 }
